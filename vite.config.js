051f6c837a32b24d026bd4c0ed0cb76dbf0c488a
Long <kieulong1401@gmail.com>
2023-09-29 10:21:01 +0900

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: 'https://kieulong1401.github.io/tiktok_clone',
 })
