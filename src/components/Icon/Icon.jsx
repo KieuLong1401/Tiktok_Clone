65c1cbfbca22e73b4897c4580e37e0e862781889
Long <kieulong1401@gmail.com>
2023-10-29 16:55:52 +0900

    some components adjustment

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -4,29 +4,32 @@ import { forwardRef, useEffect, useState } from 'react'
 
 import PropTypes from 'prop-types'
 
-const Icon = forwardRef(({ child, triggered, trigAt, trigOnClick, link = true, ...props }, ref) => {
-    const currentPath = useLocation().pathname
-    const [isTrig, setIsTrig] = useState(trigAt ? trigAt == currentPath : false)
+const Icon = forwardRef(
+    ({ className, child, triggered, trigAt, trigOnClick, link = true, ...props }, ref) => {
+        const currentPath = useLocation().pathname
+        const [isTrig, setIsTrig] = useState(trigAt ? trigAt == currentPath : false)
 
-    var Tag = 'button'
-    if (trigAt && link) {
-        Tag = 'a'
+        var Tag = 'button'
+        if (trigAt && link) {
+            Tag = 'a'
+        }
+
+        useEffect(() => {
+            setIsTrig(trigAt ? trigAt == currentPath : false)
+        }, [currentPath])
+
+        return (
+            <Tag
+                className={className}
+                ref={ref}
+                {...props}
+                href={trigAt && link ? trigAt : ''}
+                onClick={trigOnClick ? () => setIsTrig(!isTrig) : () => {}}>
+                {!isTrig ? child : triggered}
+            </Tag>
+        )
     }
-
-    useEffect(() => {
-        setIsTrig(trigAt ? trigAt == currentPath : false)
-    }, [currentPath])
-
-    return (
-        <Tag
-            ref={ref}
-            {...props}
-            href={trigAt && link ? trigAt : ''}
-            onClick={trigOnClick ? () => setIsTrig(!isTrig) : () => {}}>
-            {!isTrig ? child : triggered}
-        </Tag>
-    )
-})
+)
 
 Icon.propTypes = {
     child: PropTypes.node.isRequired,
